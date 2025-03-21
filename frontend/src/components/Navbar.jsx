4fd7d2125a246855a7dc0de9919c6cbcbf46b0d8
P-Jaswanth <jeshujeshu02@gmail.com>
2025-03-21 23:59:27 +0530

    'login'

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,146 +4,129 @@ import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 
 const Navbar = () => {
-    const [visible, setVisibility] = useState(false);
-    const {setShowSearch} = useContext(ShopContext);
+  const [visible, setVisibility] = useState(false);
+  const { setShowSearch, getCartCount } = useContext(ShopContext);
 
-    return (
-        <div className="flex items-center justify-between py-5 font-medium">
-            <Link to="/">
-                <img className="w-36" src={assets.logo} alt="ECO Logo" />
-            </Link>
+  return (
+    <div className="flex items-center justify-between py-5 font-medium">
+      <Link to="/">
+        <img className="w-36" src={assets.logo} alt="ECO Logo" />
+      </Link>
 
-            <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
-                <NavLink to="/" className="flex flex-col items-center gap-1">
-                    <p>HOME</p>
-                    <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-                </NavLink>
+      <ul className="hidden sm:flex gap-5 text-sm text-gray-700">
+        <NavLink to="/" className="flex flex-col items-center gap-1">
+          <p>HOME</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-                <NavLink
-                    to="/collection"
-                    className="flex flex-col items-center gap-1"
-                >
-                    <p>COLLECTIONS</p>
-                    <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-                </NavLink>
+        <NavLink to="/collection" className="flex flex-col items-center gap-1">
+          <p>COLLECTIONS</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-                <NavLink
-                    to="/about"
-                    className="flex flex-col items-center gap-1"
-                >
-                    <p>ABOUT US</p>
-                    <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-                </NavLink>
+        <NavLink to="/about" className="flex flex-col items-center gap-1">
+          <p>ABOUT US</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
 
-                <NavLink
-                    to="/contact"
-                    className="flex flex-col items-center gap-1"
-                >
-                    <p>CONTACT US</p>
-                    <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
-                </NavLink>
-            </ul>
+        <NavLink to="/contact" className="flex flex-col items-center gap-1">
+          <p>CONTACT US</p>
+          <hr className="w-2/4 border-none h-[1.5px] bg-gray-700 hidden" />
+        </NavLink>
+      </ul>
 
-            <div className="flex items-center gap-6">
-                <img 
-                    onClick={() => setShowSearch(true)}
-                    src={assets.search_icon}
-                    className="w-5 cursor-pointer"
-                    alt="icon-search"
-                />
+      <div className="flex items-center gap-6">
+        <img
+          onClick={() => setShowSearch(true)}
+          src={assets.search_icon}
+          className="w-5 cursor-pointer"
+          alt="icon-search"
+        />
 
-                <div className="group relative">
-                    <img
-                        className="w-5 cursor-pointer"
-                        src={assets.profile_icon}
-                        alt="profile_pic"
-                    />
+        <div className="group relative">
+          <Link to="/login">
+            <img
+              className="w-5 cursor-pointer"
+              src={assets.profile_icon}
+              alt="profile_pic"
+            />
+          </Link>
 
-                    <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
-                        <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-                            <p className="cursor-pointer hover:text-black">
-                                My Profile
-                            </p>
-                            <p className="cursor-pointer hover:text-black">
-                                Orders
-                            </p>
-                            <p className="cursor-pointer hover:text-black">
-                                Logout
-                            </p>
-                        </div>
-                    </div>
-                </div>
-
-                <Link to="/cart" className="relative">
-                    <img
-                        src={assets.cart_icon}
-                        className="w-5 min-w-5"
-                        alt="cart_icon"
-                    />
-                    <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
-                        10{" "}
-                    </p>
-                </Link>
-
-                <img
-                    onClick={() => setVisibility(true)}
-                    className="w-5 cursor-pointer sm:hidden"
-                    src={assets.menu_icon}
-                />
+          <div className="group-hover:block hidden absolute dropdown-menu right-0 pt-4">
+            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
+              <p className="cursor-pointer hover:text-black">My Profile</p>
+              <p className="cursor-pointer hover:text-black">Orders</p>
+              <p className="cursor-pointer hover:text-black">Logout</p>
             </div>
-
-            {/* { sidebar menu for small screens} */}
-
-            <div
-                className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
-                    visible ? "w-full" : "w-0"
-                }`}
-            >
-                <div className="flex flex-col text-gray-600">
-                    <div
-                        onClick={() => setVisibility(false)}
-                        className="flex items-center gap-4 p-3 cursor-pointer"
-                    >
-                        <img
-                            className="h-4 rotate-180"
-                            src={assets.dropdown_icon}
-                            alt="back_arrow"
-                        />
-                        <p>Back</p>
-                    </div>
-
-                    <NavLink
-                        onClick={() => setVisibility(false)}
-                        className="py-2 pl-6 border-t border-black/50"
-                        to="/"
-                    >
-                        HOME
-                    </NavLink>
-                    <NavLink
-                        onClick={() => setVisibility(false)}
-                        className="py-2 pl-6 border-t border-black/50"
-                        to="/collection"
-                    >
-                        COLLECTIONS
-                    </NavLink>
-                    <NavLink
-                        onClick={() => setVisibility(false)}
-                        className="py-2 pl-6 border-t border-black/50"
-                        to="/about"
-                    >
-                        ABOUT US
-                    </NavLink>
-                    <NavLink
-                        onClick={() => setVisibility(false)}
-                        className="py-2 pl-6 border border-black/50"
-                        to="/contact"
-                    >
-                        CONTACT US
-                    </NavLink>
-                </div>
-            </div>
+          </div>
         </div>
-    );
+
+        <Link to="/cart" className="relative">
+          <img src={assets.cart_icon} className="w-5 min-w-5" alt="cart_icon" />
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
+            {getCartCount()}
+          </p>
+        </Link>
+
+        <img
+          onClick={() => setVisibility(true)}
+          className="w-5 cursor-pointer sm:hidden"
+          src={assets.menu_icon}
+        />
+      </div>
+
+      {/* { sidebar menu for small screens} */}
+
+      <div
+        className={`absolute top-0 right-0 bottom-0 overflow-hidden bg-white transition-all ${
+          visible ? "w-full" : "w-0"
+        }`}
+      >
+        <div className="flex flex-col text-gray-600">
+          <div
+            onClick={() => setVisibility(false)}
+            className="flex items-center gap-4 p-3 cursor-pointer"
+          >
+            <img
+              className="h-4 rotate-180"
+              src={assets.dropdown_icon}
+              alt="back_arrow"
+            />
+            <p>Back</p>
+          </div>
+
+          <NavLink
+            onClick={() => setVisibility(false)}
+            className="py-2 pl-6 border-t border-black/50"
+            to="/"
+          >
+            HOME
+          </NavLink>
+          <NavLink
+            onClick={() => setVisibility(false)}
+            className="py-2 pl-6 border-t border-black/50"
+            to="/collection"
+          >
+            COLLECTIONS
+          </NavLink>
+          <NavLink
+            onClick={() => setVisibility(false)}
+            className="py-2 pl-6 border-t border-black/50"
+            to="/about"
+          >
+            ABOUT US
+          </NavLink>
+          <NavLink
+            onClick={() => setVisibility(false)}
+            className="py-2 pl-6 border border-black/50"
+            to="/contact"
+          >
+            CONTACT US
+          </NavLink>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
